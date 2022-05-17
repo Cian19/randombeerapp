9e2b890ea2325e93748527fb1b56e98b81fbfa8a
Cian19 <97292847+Cian19@users.noreply.github.com>
2022-05-17 17:17:08 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "asdf1234",
+    host: "yourhost",
+    user: "user",
+    password: "password",
     database: "beerdb"
 });
 
